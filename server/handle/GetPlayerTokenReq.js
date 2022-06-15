70cae2571780e424e853cc318bda1697daa735dd
RDsatan <RDsatan@outlook.com>
2022-06-15 15:32:54 +0800

    add handle example GetPlayerTokenReq

---
+++ server/handle/GetPlayerTokenReq.js
@@ -0,0 +1,18 @@
+module.exports = {//模块导出
+    async execute(dataUtil,protobuf,protobuff,jsondata,kcpobj,keyBuffer,player,world){
+        packetIdName="GetPlayerTokenRsp";
+        var packetID=dataUtil.getPacketIDByProtoName(packetIdName);
+        var GetPlayerTokenRsp = {
+            "uid": player.uid,
+            "token": protobuff["accountToken"],
+            "accountType": protobuff["accountType"],
+            "accountUid": player.uid.toString(),
+            "secretKeySeed": 0,
+            "platformType": protobuff["platformType"],
+        };
+        var data = await dataUtil.objToProtobuffer(GetPlayerTokenRsp,packetID);
+        await kcpobj.send(await dataUtil.dataToPacket(data,packetID,keyBuffer));
+        
+        console.log(`[${packetIdName}] SENT BACK\n${JSON.stringify(GetPlayerTokenRsp,null,"\t")}`);
+    }
+}
