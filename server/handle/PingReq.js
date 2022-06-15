af26c5ad515cda2abdb505c4b93ed5f8854b0c21
RDsatan <RDsatan@outlook.com>
2022-06-15 15:34:05 +0800

    add handle example PingReq

---
+++ server/handle/PingReq.js
@@ -0,0 +1,13 @@
+module.exports = {
+    async execute(dataUtil,protobuf,protobuff,jsondata,kcpobj,keyBuffer,player,world){
+        var packetIdName="PingRsp";
+        var packetID=dataUtil.getPacketIDByProtoName(packetIdName);
+        var PingRsp = {
+            clientTime: protobuff["clientTime"],
+            ueTime: protobuff["ueTime"]
+        }
+        var data = await dataUtil.objToProtobuffer(PingRsp,packetID);
+        await kcpobj.send(await dataUtil.dataToPacket(data,packetID,keyBuffer));
+        //console.log(`[${packetIdName}] SENT BACK`);
+    }
+}
